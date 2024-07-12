6ede099d5c66c837facd4dd0bb0ce7f60eca20bc
saadb <saadbasheer0@gmail.com>
2024-07-12 08:06:21 +0530

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
-import Head from 'next/head'
+
 
 export default function Home() {
   return (
-    <div style={{ height: '100vh', width: '100vw' }}>
-      <Head>
-        <title>My Resume</title>
-        <meta name="description" content="My professional resume" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <iframe 
-        src="/resume.pdf" 
-        style={{ width: '100%', height: '100%', border: 'none' }}
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <iframe
+        src="/resume.pdf"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+          overflow: "hidden",
+        }}
       />
     </div>
-  )
+  );
 }
