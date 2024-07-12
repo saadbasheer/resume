3b6c6f730b862220317d0881889e8a68ef3b3784
saadb <saadbasheer0@gmail.com>
2024-07-12 08:37:51 +0530

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function PDFViewer() {
           width="100%"
           height="100%"
         >
-          <p>It appears you don't have a PDF plugin for this browser. 
+          <p>It appears you don{"'"}t have a PDF plugin for this browser. 
           You can <a href="/resume.pdf">click here to download the PDF file.</a></p>
         </object>
       </div>
